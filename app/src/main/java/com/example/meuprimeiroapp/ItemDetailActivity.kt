783b4ea85fa1506e7b91eedd8bbefe036fab3632
Patrick <rs108954@gmail.com>
2026-03-03 22:25:30 -0300

    carregando detalhe do item sem a localização

--- app/src/main/java/com/example/meuprimeiroapp/ItemDetailActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/ItemDetailActivity.kt
@@ -3,18 +3,75 @@ package com.example.meuprimeiroapp
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.meuprimeiroapp.databinding.ActivityItemDetailBinding
+import com.example.meuprimeiroapp.model.Item
+import com.example.meuprimeiroapp.service.RetrofitClient
+import com.example.meuprimeiroapp.service.safeApiCall
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.example.meuprimeiroapp.service.Result
+import com.example.meuprimeiroapp.ui.loadUrl
 
 class ItemDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityItemDetailBinding
+
+    private lateinit var item: Item
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_item_detail)
+        binding = ActivityItemDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+        setupView()
+        loadItem()
     }
 
-    companion object{
+    private fun setupView() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    private fun loadItem() {
+        val itemId = intent.getStringExtra(ARG_ID) ?: ""
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = safeApiCall { RetrofitClient.itemApiService.getItem(itemId) }
+
+            withContext(Dispatchers.Main) {
+                when (result) {
+                    is Result.Success -> {
+                        item = result.data
+                        handleSuccess()
+                    }
+                    is Result.Error -> {
+                        Toast.makeText(
+                            this@ItemDetailActivity,
+                            "Erro ao buscar o getItem(itemId)",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleSuccess()
+    {
+        binding.name.text = item.value.fullName
+        binding.age.text = getString(R.string.item_age, item.value.age)
+        binding.profession.setText(item.value.profession)
+        binding.image.loadUrl(item.value.imageUrl)
+    }
+
+
+    companion object {
         const val ARG_ID = "arg_id"
 
         fun newIntent(context: Context, itemId: String): Intent {
